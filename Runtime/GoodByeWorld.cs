1fd7769c61ef0f684ad08d0b9ad195b0d7eda78d
장희용 <ralf1204@com2us.com>
2023-01-19 14:31:04 +0900

    a

---
+++ Runtime/GoodByeWorld.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoodByeWorld : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Good bye world");
+    }
+}
